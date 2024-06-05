37e4d067008437146c1db047269fdaba5a3ac7bd
Rodrigo Nakata <153712774+nakatadev@users.noreply.github.com>
2024-06-05 15:12:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+var ctxPH = document.getElementsByClassName("ph__chart");
+new Chart(ctxPH, {
+    type: 'bar',
+    data: {
+        labels: ["Vermelha do centro","Itamambuca","Pereque-Açu","Prumirim"],
+        datasets: [{
+            label: "pH atual",
+            data: [6, 7, 8, 6],
+            borderWidth: 2,
+            backgroundColor: ["Red","Blue","Grey","White"],
+        },
+        {
+            label: "pH adequado",
+            data: [7, 7, 7, 7],
+            borderWidth: 2,
+            backgroundColor: "Green"
+        }
+    ]
+    }
+});
+
+var ctxOX = document.getElementsByClassName("OX__chart");
+new Chart(ctxOX, {
+    type: 'bar',
+    data: {
+        labels: ["Vermelha do centro","Itamambuca","Pereque-Açu","Prumirim"],
+        datasets: [{
+            label: "Oxigênio atual",
+            data: [7, 6, 8, 9],
+            borderWidth: 2,
+            backgroundColor: ["Red","Blue","Grey","White"],
+        },
+        {
+            label: "Oxigênio adequado",
+            data: [7,7,7,7],
+            borderWidth: 2,
+            backgroundColor: "Green"
+        }
+    ]
+
+    }
+})
+
+var ctxSal = document.getElementsByClassName("sal__chart");
+new Chart(ctxSal, {
+    type: 'bar',
+    data: {
+        labels: ["Vermelha do centro","Itamambuca","Pereque-Açu","Prumirim"],
+        datasets: [{
+            label: "Salinidade atual (ppt)",
+            data: [32,33,35,32],
+            borderWidth: 2,
+            backgroundColor: ["Red","Blue","Grey","White"],
+        },
+        {
+            label: "Salinidade adequada (ppt)",
+            data: [33,33,33,33],
+            borderWidth: 2,
+            backgroundColor: ["Green"],
+        }
+    ]
+    }
+})
+
+var ctxTemp = document.getElementsByClassName("temp__chart")
+new Chart(ctxTemp, {
+    type: 'bar',
+    data: {
+        labels: ["Vermelha do centro","Itamambuca","Pereque-Açu","Prumirim"],
+        datasets: [{
+            label: "Temperatura atual",
+            data: [26,25,28,29],
+            borderWidth: 2,
+            backgroundColor: ["Red","Blue","Grey","White"],
+        },
+        {
+            label: "Temperatura adequada",
+            data: [28,28,28,28],
+            borderWidth: 2,
+            backgroundColor: ["green"],
+        }
+    ]
+    }
+})
